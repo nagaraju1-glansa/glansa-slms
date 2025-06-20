48507d86eda1db9776791cd8081fc272057361e5
Nagaraju <im.nagraaz@gmail.com>
2025-06-20 18:57:28 +0530

    2

--- src/pages/ApiConfig/ApiConfig.js
+++ src/pages/ApiConfig/ApiConfig.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://127.0.0.1:8000/api';
+export const API_BASE_URL = 'https://api.glansadesigns.com/glansafin/api';
 export const getToken = () => localStorage.getItem('token');
 export const getCompanyId = () => localStorage.getItem('companyId');
 export const clearSession = () => localStorage.clear();
